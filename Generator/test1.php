9c5cfd01cbe607c143375344de782eccb5207444
pengweiwei <pengweiwei@ttyun.com>
2017-12-12 17:16:15 +0800

    chore:del Reflection dir

--- Generator/test1.php
+++ Generator/test1.php
@@ -1,158 +1,47 @@
 <?php
-class Task {
-    protected $taskId;
-    protected $coroutine;
-    protected $sendValue = null;
-    protected $beforeFirstYield = true;
+/**
+ * 测试数据库连接是否正常
+ * 
+ */
+$conns = [
+    [
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'username' => 'root',
+        'password' => 'admin',
+    ],
+    [
+        'host' => '127.0.0.1',
+        'port' => '13306',
+        'username' => 'root',
+        'password' => 'admin',
+    ],
+    [
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'username' => 'root',
+        'password' => 'admin',
+    ],
+];
 
-    public function __construct($taskId, Generator $coroutine) {
-        $this->taskId =$taskId;
-        $this->coroutine = $coroutine;
-    }
-
-    public function getTaskId() {
-        return $this->taskId;
-    }
-
-    public function setSendValue($sendValue) {
-        $this->sendValue = $sendValue;
-    }
-
-    public function run() {
-        if ($this->beforeFirstYield) {
-            $this->beforeFirstYield = false;
-            return $this->coroutine->current();
-        } else {
-            $retval = $this->coroutine->send($this->sendValue);
-            $this->sendValue = null;
-            return $retval;
-        }
-    }
-
-    public function isFinished() {
-        return !$this->coroutine->valid();
-    }
+$results = [];
+foreach (conn($conns) as $res) {
+    // 收集每次连接测试结果
+    $results[] = $res;
 }
 
-
-class Scheduler {
-    protected $maxTaskId = 0;
-    protected $taskMap = [];
-    protected $taskQueue;
-
-    public function __construct() {
-        $this->taskQueue = NEW SplQueue();
-    }
-
-    public function newTask(Generator $coroutine) {
-        $tid = ++$this->maxTaskId;
-        $task = new Task($tid, $coroutine);
-        $this->taskMap[$tid] = $task;
-        $this->schedule($task);
-        return $tid;
-    }
-
-    public function killTask($tid) {
-        if (!isset($this->taskMap[$tid])) {
-            return false;
-        }
-
-        unset($this->taskMap[$tid]);
-
-        foreach ($this->taskQueue as $i => $task) {
-            if ($task->getTaskId() === $tid) {
-                unset($this->taskQueue[$i]);
-                break;
-            }
-        }
-        return true;
-    }
-
-    public function schedule(Task $task) {
-        $this->taskQueue->enqueue($task);
-    }
-
-    public function run() {
-        while (!$this->taskQueue->isEmpty()) {
-            $task = $this->taskQueue->dequeue();
-            $retval = $task->run();
-
-            if ($retval instanceof SystemCall) {
-                $retval($task, $this);
-                continue;
-            }
-
-            if ($task->isFinished()) {
-                unset($this->taskMap[$task->getTaskId()]);
-            } else {
-                $this->schedule($task);
-            }
+function conn($conns){
+    foreach ($conns as $row) {
+        $con = @mysqli_connect($row['host'].":".$row['port'],$row['username'],$row['password']);
+        if (!$con){
+            // 连接不成功
+            yield $row['host'] . ":" . $row['port'] . "\n";
+        }else{
+            // 连接成功
+            mysqli_close($con);
+            yield "0\n";
         }
     }
 }
 
-class SystemCall {
-    protected $callback;
-
-    public function __construct(callable $callback) {
-        $this->callback = $callback;
-    }
-
-    public function __invoke(Task $task, Scheduler $scheduler) {
-        $callback = $this->callback;
-        return $callback($task, $scheduler);
-    }
-}
-
-function getTaskId() {
-    return new SystemCall(function(Task $task, Scheduler $scheduler) {
-        $task->setSendValue($task->getTaskId());
-        $scheduler->schedule($task);
-    });
-}
-
-function newTask(Generator $coroutine) {
-    return new SystemCall(
-        function(Task $task, Scheduler $scheduler) use ($coroutine) {
-            $task->setSendValue($scheduler->newTask($coroutine));
-            $scheduler->schedule($task); // 为什么这里又要插入一次？
-        }
-    );
-}
-
-function killTask($tid) {
-    return new SystemCall(
-        function(Task $task, Scheduler $scheduler) use ($tid) {
-            $task->setSendValue($scheduler->killTask($tid));
-            $scheduler->schedule($task);
-        }
-    );
-}
-function childTask() {
-    $tid = (yield getTaskId());
-    while (true) {
-        echo "Child task $tid still alive!\n";
-        yield;
-    }
-}
-
-function task() {
-    $tid = (yield getTaskId());
-    $childTid = (yield newTask(childTask()));
-
-    for ($i = 1; $i <= 6; ++$i) {
-        echo "This is task $tid iteration $i.\n";
-        yield;
-
-        if ($i ==3) {
-            yield killTask($childTid);
-        }
-    }
-}
-
-$scheduler = new Scheduler;
-
-$scheduler->newTask(task());
-
-$scheduler->run();
-?>
+print_r($results);
